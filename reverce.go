f8d74b1445a5643ddc3966772d8e087a494414b4
Prokopevs <prokopev_savva@mail.ru>
2024-01-22 19:18:00 +0300

    binarySerch

--- reverce.go
+++ reverce.go
@@ -1,29 +1,29 @@
-package main 
+// package main 
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 	
-)
+// )
 
-func main () {
-	slice := []int{1, 3, 4, 9, 4,7}
+// func main () {
+// 	slice := []int{1, 3, 4, 9, 4,7}
 
-	var value int
-	fmt.Scan(&value)
-	var num int
-	if len(slice)%2 == 0 {
-		num = len(slice)/2
-	} else {
-		num = (len(slice)-1)/2
-	}
+// 	var value int
+// 	fmt.Scan(&value)
+// 	var num int
+// 	if len(slice)%2 == 0 {
+// 		num = len(slice)/2
+// 	} else {
+// 		num = (len(slice)-1)/2
+// 	}
 
-	for i:= 0; i<num; i++ {
-		temp := slice[i]
-		slice[i] = slice[len(slice)-i-1]
-		slice[len(slice)-i-1] = temp
-	}
-	fmt.Println(slice)
-	fmt.Println(value)
+// 	for i:= 0; i<num; i++ {
+// 		temp := slice[i]
+// 		slice[i] = slice[len(slice)-i-1]
+// 		slice[len(slice)-i-1] = temp
+// 	}
+// 	fmt.Println(slice)
+// 	fmt.Println(value)
 
-}
+// }
 
