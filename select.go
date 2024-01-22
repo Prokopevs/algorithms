c5e0b4038e10d83a9bdd77dbed3593cbb447e032
Prokopevs <prokopev_savva@mail.ru>
2024-01-22 17:29:27 +0300

    slices

--- select.go
+++ select.go
@@ -1,27 +1,27 @@
-package main 
+// package main 
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main () {
-	array := [5]int{1, 3, 4, 9, 4}
+// func main () {
+// 	array := [5]int{1, 3, 4, 9, 4}
 
-	for i := 0; i < len(array); i++ {
-		minIndex := i
-		for j := i; j < len(array)-1; j++ {
-			if array[minIndex] > array[j+1] {
-				minIndex = j+1
-			}
-		}
+// 	for i := 0; i < len(array); i++ {
+// 		minIndex := i
+// 		for j := i; j < len(array)-1; j++ {
+// 			if array[minIndex] > array[j+1] {
+// 				minIndex = j+1
+// 			}
+// 		}
 
-		if i != minIndex {
-			temp := array[i]
-			array[i] = array[minIndex]
-			array[minIndex] = temp
-		}
+// 		if i != minIndex {
+// 			temp := array[i]
+// 			array[i] = array[minIndex]
+// 			array[minIndex] = temp
+// 		}
 		
-	}
+// 	}
 
-	fmt.Println(array)
-}
+// 	fmt.Println(array)
+// }
---
+++ slices.go
@@ -0,0 +1,32 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main () {
+
+	// удалить с конца
+	// slice := []int{1, 3, 4, 9, 4}
+	// slice = slice[0:len(slice)-1]
+	// fmt.Println(slice)
+
+
+	// удалить с начала
+	// slice2 := []int{1, 3, 4, 9, 4}
+	// slice2 = slice2[1:] 
+	// fmt.Println(slice2)
+
+	// удалить с середины
+	// slice3 := []int{1, 3, 4, 9, 4}
+	// n := 3
+	// slice3C := append(slice3[0:n], slice3[n+1:]...)
+	// fmt.Println(slice3C)
+
+	//вставка в начало
+	// slice4 := []int{1, 3, 4, 9, 4}
+	// n := 0
+	// slice4C := append(slice4[:n+1], slice4[n:]...)
+	// slice4C[0] = 10
+	// fmt.Println(slice4C)
+}
