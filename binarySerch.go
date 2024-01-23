72ba2130f7af8e548d36565ccb76409d1a681dcf
Prokopevs <prokopev_savva@mail.ru>
2024-01-23 20:20:48 +0300

    linkedList

--- binarySerch.go
+++ binarySerch.go
@@ -1,32 +1,32 @@
-package main 
+// package main 
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 	
-)
+// )
 
-func main () {
-	slice := []int{1, 3, 4, 5, 6, 7, 8}
-	find := 5
-	min := 0
-	high := len(slice)-1
+// func main () {
+// 	slice := []int{1, 3, 4, 5, 6, 7, 8}
+// 	find := 5
+// 	min := 0
+// 	high := len(slice)-1
 
-	res := false
-	for i := 0; i < len(slice); i++ { 
-		mid := (min + high)/2
-		if find == slice[mid] {
-			res = true
-			break
-		}
-		if find > slice[mid] {
-			min = mid + 1  
-			continue 
-		}
-		if find < slice[mid] {
-			high = mid -1
-			continue
-		} 
-	}
-	fmt.Println(res)
-}
+// 	res := false
+// 	for i := 0; i < len(slice); i++ { 
+// 		mid := (min + high)/2
+// 		if find == slice[mid] {
+// 			res = true
+// 			break
+// 		}
+// 		if find > slice[mid] {
+// 			min = mid + 1  
+// 			continue 
+// 		}
+// 		if find < slice[mid] {
+// 			high = mid -1
+// 			continue
+// 		} 
+// 	}
+// 	fmt.Println(res)
+// }
 
---
+++ linkedList.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	data string
+	next *Node
+}
+
+var head *Node = &Node{data: "LU", next: nil}
+
+func main() {
+	var ff *Node = &Node{data: "GW", next: nil}
+	head.next = ff
+
+	var tail *Node = &Node{data: "RT", next: nil}
+	ff.next = tail
+
+	p := head
+	for {
+		fmt.Println(p.data)
+		if p.next == nil {
+			break
+		}
+		p = p.next
+	}
+}
