d399e7222e4d6ce1e5e9419da471c6c2d331ac2f
Prokopevs <prokopev_savva@mail.ru>
2024-01-24 17:31:53 +0300

    slices

--- linkedList.go
+++ linkedList.go
@@ -1,29 +1,29 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-type Node struct {
-	data string
-	next *Node
-}
+// type Node struct {
+// 	data string
+// 	next *Node
+// }
 
-var head *Node = &Node{data: "LU", next: nil}
+// var head *Node = &Node{data: "LU", next: nil}
 
-func main() {
-	var ff *Node = &Node{data: "GW", next: nil}
-	head.next = ff
+// func main() {
+// 	var ff *Node = &Node{data: "GW", next: nil}
+// 	head.next = ff
 
-	var tail *Node = &Node{data: "RT", next: nil}
-	ff.next = tail
+// 	var tail *Node = &Node{data: "RT", next: nil}
+// 	ff.next = tail
 
-	p := head
-	for {
-		fmt.Println(p.data)
-		if p.next == nil {
-			break
-		}
-		p = p.next
-	}
-}
+// 	p := head
+// 	for {
+// 		fmt.Println(p.data)
+// 		if p.next == nil {
+// 			break
+// 		}
+// 		p = p.next
+// 	}
+// }
--- slices.go
+++ slices.go
@@ -1,32 +1,60 @@
-// package main 
+package main 
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
-// func main () {
+func main () {
 
-// 	удалить с конца
-// 	slice := []int{1, 3, 4, 9, 4}
-// 	slice = slice[0:len(slice)-1]
-// 	fmt.Println(slice)
+	// удалить с конца
+	// slice := []int{1, 3, 4, 9, 4}
+	// slice = slice[0:len(slice)-1]
+	// fmt.Println(slice)
 
 
-// 	удалить с начала
-// 	slice2 := []int{1, 3, 4, 9, 4}
-// 	slice2 = slice2[1:] 
-// 	fmt.Println(slice2)
+	// удалить с начала
+	// slice2 := []int{1, 3, 4, 9, 4}
+	// slice2 = slice2[1:] 
+	// fmt.Println(slice2)
 
-// 	удалить с середины
-// 	slice3 := []int{1, 3, 4, 9, 4}
-// 	n := 3
-// 	slice3C := append(slice3[0:n], slice3[n+1:]...)
-// 	fmt.Println(slice3C)
+	// удалить с середины
+	// slice3 := []int{1, 3, 4, 9, 4}
+	// n := 3
+	// slice3C := append(slice3[0:n], slice3[n+1:]...)
+	// fmt.Println(slice3C)
 
-// 	вставка в начало
-// 	slice4 := []int{1, 3, 4, 9, 4}
-// 	n := 0
-// 	slice4C := append(slice4[:n+1], slice4[n:]...)
-// 	slice4C[0] = 10
-// 	fmt.Println(slice4C)
-// }
+	// вставка в начало
+	// slice4 := []int{1, 3, 4, 9, 4}
+	// n := 0
+	// slice4C := append(slice4[:n+1], slice4[n:]...)
+	// slice4C[0] = 10
+	// fmt.Println(slice4C)
+
+
+	nums := make([]int, 1, 2)
+	fmt.Println(nums)
+
+	// appendSlice(nums, 1024)
+	// fmt.Println(nums[:2])
+
+	// mutateSlice(nums, 0, 512)
+	// fmt.Println(nums)
+
+	a := append(nums, 1)
+	fmt.Println(a)
+	fmt.Println(nums[:2])
+
+
+}
+
+func appendSlice (sl []int, val int) {
+	sl = append(sl, val)
+}
+
+func mutateSlice (sl []int, idx, val int) {
+	sl[idx] = val
+}
+
+
+
+
