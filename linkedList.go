4300c065d00a98469f77763c462e8471a6cf5b1c
Prokopevs <prokopev_savva@mail.ru>
2024-01-24 20:28:18 +0300

    Add in index LinkedList

--- linkedList.go
+++ linkedList.go
@@ -1,44 +1,44 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-type Node struct {
-	data string
-	next *Node
-}
+// type Node struct {
+// 	data string
+// 	next *Node
+// }
 
-var head *Node = &Node{data: "LU", next: nil}
-var tail *Node = new(Node)
+// var head *Node = &Node{data: "LU", next: nil}
+// var tail *Node = new(Node)
 
-func main() {
-	var ff *Node = &Node{data: "GW", next: nil}
-	head.next = ff
+// func main() {
+// 	var ff *Node = &Node{data: "GW", next: nil}
+// 	head.next = ff
 
-	var gg *Node = &Node{data: "RT", next: nil}
-	ff.next = gg
+// 	var gg *Node = &Node{data: "RT", next: nil}
+// 	ff.next = gg
 
-	tail = &Node{data: "HH", next: nil}
-	gg.next = tail
+// 	tail = &Node{data: "HH", next: nil}
+// 	gg.next = tail
 
-	add("JJ")
+// 	add("JJ")
 
-	p := head
-	for {
-		fmt.Println(p.data)
-		if p.next == nil {
-			break
-		}
-		p = p.next
-	}
-}
+// 	p := head
+// 	for {
+// 		fmt.Println(p.data)
+// 		if p.next == nil {
+// 			break
+// 		}
+// 		p = p.next
+// 	}
+// }
 
-func add (data string) {
-	var newN *Node = &Node{data: data, next: nil}
-	tail.next = newN
+// func add (data string) {
+// 	var newN *Node = &Node{data: data, next: nil}
+// 	tail.next = newN
 
-	tail = newN
-}
+// 	tail = newN
+// }
 
 
---
+++ linkedListM.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	data string
+	next *Node
+}
+
+
+var tail *Node = new(Node)
+
+
+func main() {
+	var head *Node = &Node{data: "LU", next: nil}
+	tail = head
+
+	tail = tail.add("GW", tail)
+	tail = tail.add("OM", tail)
+
+	head.insert("ie", 1)
+
+	p := head
+	for {
+		fmt.Println(p.data)
+		if p.next == nil {
+			break
+		}
+		p = p.next
+	}
+}
+
+
+func (s *Node) add(data string, tail *Node) (t *Node) {
+	var newN *Node = &Node{data: data, next: nil}
+	tail.next = newN
+	tail = newN
+
+	return tail
+} 
+
+func (s *Node) insert(data string, index int) {
+	var p = s
+	var i = 0
+
+	
+	for {
+		if p.next == nil || i >= index-1 {
+			fmt.Println((i == index-1))
+			break
+		}
+		p = p.next
+		i++
+	}
+
+
+	var newN *Node = &Node{data: data, next: p.next}
+	p.next = newN
+} 
+
