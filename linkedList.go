085089caa55adfaa97892ddc4454bcdb7c1f1776
Prokopevs <prokopev_savva@mail.ru>
2024-01-24 18:34:17 +0300

    linkedList

--- linkedList.go
+++ linkedList.go
@@ -1,29 +1,44 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
-// type Node struct {
-// 	data string
-// 	next *Node
-// }
+type Node struct {
+	data string
+	next *Node
+}
 
-// var head *Node = &Node{data: "LU", next: nil}
+var head *Node = &Node{data: "LU", next: nil}
+var tail *Node = new(Node)
 
-// func main() {
-// 	var ff *Node = &Node{data: "GW", next: nil}
-// 	head.next = ff
+func main() {
+	var ff *Node = &Node{data: "GW", next: nil}
+	head.next = ff
 
-// 	var tail *Node = &Node{data: "RT", next: nil}
-// 	ff.next = tail
+	var gg *Node = &Node{data: "RT", next: nil}
+	ff.next = gg
 
-// 	p := head
-// 	for {
-// 		fmt.Println(p.data)
-// 		if p.next == nil {
-// 			break
-// 		}
-// 		p = p.next
-// 	}
-// }
+	tail = &Node{data: "HH", next: nil}
+	gg.next = tail
+
+	add("JJ")
+
+	p := head
+	for {
+		fmt.Println(p.data)
+		if p.next == nil {
+			break
+		}
+		p = p.next
+	}
+}
+
+func add (data string) {
+	var newN *Node = &Node{data: data, next: nil}
+	tail.next = newN
+
+	tail = newN
+}
+
+
