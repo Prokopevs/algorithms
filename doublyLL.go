679c7d2a0c8ee9bec2f26663dadcd9ff4177d280
Prokopevs <prokopev_savva@mail.ru>
2024-01-26 19:28:59 +0300

    lindedListM

---
+++ doublyLL.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data string
+	prev *Node
+	next *Node
+}
+
+	var head *Node = new(Node)
+	var tail *Node = new(Node)
+func main() {
+	
+	head.data = "AA"
+	head.next = nil
+	head.prev = nil
+
+	var holand *Node = &Node{data: "BB", prev: head, next: nil}
+	head.next = holand
+
+	var ronaldo *Node = &Node{data: "CC", prev: holand, next: nil}
+	holand.next = ronaldo
+
+	tail.data = "GG"
+	tail.prev = ronaldo
+	tail.next = nil
+	ronaldo.next = tail
+
+	// add to the end
+	add("MM")
+	insert("ST", 2)
+
+	p := head
+	for {
+		fmt.Println(p.data)
+		if p.next == nil {
+			break
+		} 
+		p = p.next
+	}
+}
+
+func add (data string ) {
+	var messi *Node = &Node{data: data, prev: tail, next: nil}
+	tail.next = messi
+	tail = messi
+}
+
+func insert (data string, index int) {
+	var p = head
+	var i = 0
+
+	for {
+		if p.next == nil || i >= index-1 {
+			break
+		}
+		p = p.next
+		i++
+	}
+
+	var newEl *Node = &Node{data: data, prev: nil, next: nil}
+
+	newEl.next = p.next 
+	p.next = newEl
+
+	newEl.prev = p
+	newEl.next.prev = newEl
+}
--- linkedListM.go
+++ linkedListM.go
@@ -1,62 +1,82 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-type Node struct {
-	data string
-	next *Node
-}
-
-
-var tail *Node = new(Node)
+// type Node struct {
+// 	data string
+// 	next *Node
+// }
 
 
-func main() {
-	var head *Node = &Node{data: "LU", next: nil}
-	tail = head
-
-	tail = tail.add("GW", tail)
-	tail = tail.add("OM", tail)
-
-	head.insert("ie", 1)
-
-	p := head
-	for {
-		fmt.Println(p.data)
-		if p.next == nil {
-			break
-		}
-		p = p.next
-	}
-}
+// var tail *Node = new(Node)
 
 
-func (s *Node) add(data string, tail *Node) (t *Node) {
-	var newN *Node = &Node{data: data, next: nil}
-	tail.next = newN
-	tail = newN
+// func main() {
+// 	var head *Node = &Node{data: "LU", next: nil}
+// 	tail = head
 
-	return tail
-} 
+// 	tail = tail.add("GW", tail)
+// 	tail = tail.add("OM", tail)
 
-func (s *Node) insert(data string, index int) {
-	var p = s
-	var i = 0
+// 	head.insert("ie", 1)
 
-	
-	for {
-		if p.next == nil || i >= index-1 {
-			fmt.Println((i == index-1))
-			break
-		}
-		p = p.next
-		i++
-	}
+// 	head.delete(2)
+
+// 	p := head
+// 	for {
+// 		fmt.Println(p.data)
+// 		if p.next == nil {
+// 			break
+// 		}
+// 		p = p.next
+// 	}
+// }
 
 
-	var newN *Node = &Node{data: data, next: p.next}
-	p.next = newN
-} 
+// func (s *Node) add(data string, tail *Node) (t *Node) {
+// 	var newN *Node = &Node{data: data, next: nil}
+// 	tail.next = newN
+// 	tail = newN
+
+// 	return tail
+// } 
+
+// func (s *Node) insert(data string, index int) {
+// 	var p = s
+// 	var i = 0
+
+// 	for {
+// 		if p.next == nil || i >= index-1 {
+// 			break
+// 		}
+// 		p = p.next
+// 		i++
+// 	}
+
+
+// 	var newN *Node = &Node{data: data, next: p.next}
+// 	p.next = newN
+// } 
+
+// func (s *Node) delete(index int) {
+// 	var p = s
+// 	var i = 0
+
+// 	for {
+// 		if p.next == nil || i >= index-1 {
+// 			break
+// 		}
+// 		p = p.next
+// 		i++
+// 	}
+
+// 	tempD := p.next
+
+// 	p.next = p.next.next
+
+// 	tempD.next = nil
+// } 
+
 
