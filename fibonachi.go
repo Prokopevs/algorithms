3943a901e31301eda8362e079cf0af5dfa818aa2
Prokopevs <prokopev_savva@mail.ru>
2024-02-13 20:32:48 +0300

    fibonachi

---
+++ fibonachi.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 7
+	fmt.Println(fib(n))
+}
+
+func fib(n int) int{
+	if n < 2 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
+}
--- vowels.go
+++ vowels.go
@@ -1,34 +1,39 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"strings"
-)
+// import (
+// 	"fmt"
+// 	"strings"
+// )
 
-func main() {
-    word := "helloaa"
-	wordToLower := strings.ToLower(word)
-	count := 0
+// func main() {
+//     word := "helloaa"
+// 	wordToLower := strings.ToLower(word)
+// 	count := 0
 
-	for i := 0; i < len(wordToLower); i++ {
-		switch string(wordToLower[i]) {
-		case "a", "e", "i", "o", "u", "y" :
-			count++
-		}
+// 	for i := 0; i < len(wordToLower); i++ {
+// 		switch string(wordToLower[i]) {
+// 		case "a", "e", "i", "o", "u", "y" :
+// 			count++
+// 		}
 
-	}
+// 	}
 
-	// for _, v := range wordToLower {
-	// 	switch v {
-	// 	case 'a', 'e', 'i', 'o', 'u', 'y' :
-	// 		count++
-	// 	}
-	// }
+// 	// for _, v := range wordToLower {
+// 	// 	switch v {
+// 	// 	case 'a', 'e', 'i', 'o', 'u', 'y' :
+// 	// 		count++
+// 	// 	}
+// 	// }
 
-	
+// 	vow := []string{"a", "e", "i", "o", "u", "y"}
+// 	c := 0
 
+// 	for _, v := range wordToLower {
+// 		if strings.ContainsAny(string(v), strings.Join(vow, "")) {
+// 			c++
+// 		}
+// 	}
 
-
-
-	fmt.Println(count)
-}
+// 	fmt.Println(count)
+// 	fmt.Println(c)
+// }
