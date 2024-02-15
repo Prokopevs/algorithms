600079ccdf557b3bf50440f9315747daf43d1883
Prokopevs <prokopev_savva@mail.ru>
2024-02-15 18:15:36 +0300

    fizzBuzz

--- fibonachi.go
+++ fibonachi.go
@@ -1,16 +1,16 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	n := 7
-	fmt.Println(fib(n))
-}
+// func main() {
+// 	n := 7
+// 	fmt.Println(fib(n))
+// }
 
-func fib(n int) int{
-	if n < 2 {
-		return n
-	}
+// func fib(n int) int{
+// 	if n < 2 {
+// 		return n
+// 	}
 
-	return fib(n-1) + fib(n-2)
-}
+// 	return fib(n-1) + fib(n-2)
+// }
