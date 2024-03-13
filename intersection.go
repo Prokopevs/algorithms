411c694dbf4dd8d529a35278b695b40343e63808
Prokopevs <prokopev_savva@mail.ru>
2024-03-13 20:19:02 +0300

    intersection, randomNumber

--- intersection.go
+++ intersection.go
@@ -1,31 +1,31 @@
-package main
+// package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 	
-)
+// )
 
 
-func main() {
-	s1 := []int{1, 3, 3, 4, 5}
-	s2 := []int{3, 3, 3, 3}
-	m1 := make(map[int]int, 0)
+// func main() {
+// 	s1 := []int{1, 3, 3, 4, 5}
+// 	s2 := []int{3, 3, 3, 3}
+// 	m1 := make(map[int]int, 0)
 
-	s3 := []int{}
+// 	s3 := []int{}
 
-	for _, v := range s1 {
-		m1[v]++
-	} 
+// 	for _, v := range s1 {
+// 		m1[v]++
+// 	} 
 
-	for _, v := range s2 {
-		if m1[v] > 0 {
-			m1[v]--
-			s3 = append(s3, v)
-		}
-	}
+// 	for _, v := range s2 {
+// 		if m1[v] > 0 {
+// 			m1[v]--
+// 			s3 = append(s3, v)
+// 		}
+// 	}
 
-	fmt.Println(s3)
-}
+// 	fmt.Println(s3)
+// }
 
 
 
---
+++ randomNumber.go
@@ -0,0 +1,21 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"math/rand"
+	
+// 	)
+
+
+// func main() {
+// 	randomNumber := rand.Intn(101)
+
+// 	fmt.Printf("Случайное число: %d\n", randomNumber)
+// }
+
+
+
+
+
+
+
