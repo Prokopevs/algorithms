0db10165aad3ef5ed698919fc39a07b4628e483f
Prokopevs <prokopev_savva@mail.ru>
2024-03-26 22:37:19 +0300

    chanel

--- threeChanel.go
+++ threeChanel.go
@@ -1,51 +1,51 @@
-package main
-import (
-	"fmt"
-	"sync"
+// package main
+// import (
+// 	"fmt"
+// 	"sync"
 	
-	)
+// 	)
 
 
-func main() {
-	var c1, c2, c3 = make(chan int, 3), make(chan int, 3), make(chan int, 3)
+// func main() {
+// 	var c1, c2, c3 = make(chan int, 3), make(chan int, 3), make(chan int, 3)
 
-	for i:=0; i<3; i++ {
-		c1 <- i
-		c2 <- i
-		c3 <- i
-	}
+// 	for i:=0; i<3; i++ {
+// 		c1 <- i
+// 		c2 <- i
+// 		c3 <- i
+// 	}
 
-	ch := crea(c1, c2, c3)
+// 	ch := crea(c1, c2, c3)
 
-	count := len(ch)
-	for i:=0; i<count; i++ {
-		fmt.Println(<-ch)
-	}
-}
+// 	count := len(ch)
+// 	for i:=0; i<count; i++ {
+// 		fmt.Println(<-ch)
+// 	}
+// }
 
-func crea(in ...<-chan int) chan int{
-	var mu sync.Mutex
-	var wg sync.WaitGroup 
-	wg.Add(3) 
-	ch := make(chan int, 9)
+// func crea(in ...<-chan int) chan int{
+// 	var mu sync.Mutex
+// 	var wg sync.WaitGroup 
+// 	wg.Add(3) 
+// 	ch := make(chan int, 9)
 
-	for i:= 0; i<len(in); i++ {
+// 	for i:= 0; i<len(in); i++ {
 		
-		go func(h <-chan int) {
-			count := len(h)
-			for i:=0; i<count; i++ {
-				mu.Lock()
-				ch <- <-h
-				mu.Unlock()
-			}
+// 		go func(h <-chan int) {
+// 			count := len(h)
+// 			for i:=0; i<count; i++ {
+// 				mu.Lock()
+// 				ch <- <-h
+// 				mu.Unlock()
+// 			}
 			
-			wg.Done()
-		}(in[i])
-	}
+// 			wg.Done()
+// 		}(in[i])
+// 	}
 
-	wg.Wait() 
-	return ch
-}
+// 	wg.Wait() 
+// 	return ch
+// }
 
 
 
